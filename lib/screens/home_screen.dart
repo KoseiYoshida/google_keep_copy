04be91c2720a2a57fdcb86883de7cd128c14570e
KoseiYoshida <koseiasengineer@gmail.com>
2021-05-09 10:10:49 +0900

    home画面の位置微調整

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,122 +13,126 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              child: Row(
-                children: [
-                  Icon(Icons.menu),
-                  // TextField(),
-                  SizedBox(
-                    width: 310,
-                  ),
-                  Icon(Icons.view_agenda_outlined),
-                  Icon(Icons.account_circle_outlined),
-                ],
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.menu),
+                    // TextField(),
+                    Row(
+                      children: [
+                        Icon(Icons.view_agenda_outlined),
+                        Icon(Icons.account_circle_outlined),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                final tile = ContentTile(content: contents[index]);
-                return GestureDetector(
-                  child: tile,
-                  onTap: () async {
-                    var editedContent = await Navigator.push<Content>(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) {
-                          return EditContentScreen(
-                            content: contents[index],
-                          );
-                        },
-                      ),
-                    );
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) {
+                  final tile = ContentTile(content: contents[index]);
+                  return GestureDetector(
+                    child: tile,
+                    onTap: () async {
+                      var editedContent = await Navigator.push<Content>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) {
+                            return EditContentScreen(
+                              content: contents[index],
+                            );
+                          },
+                        ),
+                      );
 
-                    if (editedContent != null) {
-                      setState(() {
-                        this.contents[index] = editedContent;
-                      });
-                    }
-                  },
-                );
-              },
-              itemCount: contents.length,
+                      if (editedContent != null) {
+                        setState(() {
+                          this.contents[index] = editedContent;
+                        });
+                      }
+                    },
+                  );
+                },
+                itemCount: contents.length,
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 5,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: new Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    Icons.check_box_outlined,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.brush_outlined,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.mic_none,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.image_outlined,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 5,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: new Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.check_box_outlined,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.brush_outlined,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.mic_none,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: Icon(
-                  Icons.image_outlined,
-                  color: Colors.black,
-                ),
-                onPressed: () {},
-              ),
-            ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.white,
+          child: Icon(
+            Icons.add,
+            size: 45,
+            color: Colors.red,
           ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.white,
-        child: Icon(
-          Icons.add,
-          size: 45,
-          color: Colors.red,
-        ),
-        onPressed: () async {
-          var value = await Navigator.pushNamed(context, '/add');
-          if (value != null) {
-            if (!(value is Content)) {
-              throw 'Invalid type variable passed from route /add. Passed type:${value.runtimeType}';
+          onPressed: () async {
+            var value = await Navigator.pushNamed(context, '/add');
+            if (value != null) {
+              if (!(value is Content)) {
+                throw 'Invalid type variable passed from route /add. Passed type:${value.runtimeType}';
+              }
+              var content = value as Content;
+              setState(() {
+                this.contents.add(content);
+              });
             }
-            var content = value as Content;
-            setState(() {
-              this.contents.add(content);
-            });
-          }
-        },
+          },
+        ),
       ),
     );
   }
